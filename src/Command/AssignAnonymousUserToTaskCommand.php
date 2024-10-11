81e08e96e6856679af22cca4a46c10a152447d30
Goldstein Ludivine <sneakymombox@gmail.com>
2024-10-11 17:09:09 +0200

    link to anonymous user for old tasks made by fixtures

---
+++ src/Command/AssignAnonymousUserToTaskCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class AssignAnonymousUserToTaskCommand extends Command
+{
+    protected static $defaultName = 'app:assign-anonymous-to-tasks';
+
+    private $taskRepository;
+    private $entityManager;
+
+    public function __construct(TaskRepository $taskRepository, EntityManagerInterface $entityManager)
+    {
+        parent::__construct();
+        $this->taskRepository = $taskRepository;
+        $this->entityManager = $entityManager;
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Assigner l\'utilisateur "anonyme" à toutes les tâches sans auteur.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        // Récupérer l'utilisateur anonyme
+        $anonymousUser = $this->entityManager->getRepository(User::class)->findOneBy(['username' => 'anonyme']);
+
+        if (!$anonymousUser) {
+            $io->error('L\'utilisateur "anonyme" n\'existe pas. Exécutez les fixtures d\'abord.');
+            return Command::FAILURE;
+        }
+
+        // Récupérer toutes les tâches sans auteur
+        $tasksWithoutAuthor = $this->taskRepository->findBy(['author' => null]);
+
+        foreach ($tasksWithoutAuthor as $task) {
+            $task->setAuthor($anonymousUser);
+        }
+
+        // Sauvegarder les modifications
+        $this->entityManager->flush();
+
+        $io->success('Toutes les tâches sans auteur ont été assignées à l\'utilisateur anonyme.');
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/DataFixtures/TaskFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Task;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class TaskFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+
+        // Récupérer ou créer un utilisateur "anonyme"
+        $anonymousUser = new User();
+        $anonymousUser->setUsername('anonyme');
+        $anonymousUser->setEmail('anonyme@example.com');
+        $anonymousUser->setPassword(''); // Mot de passe vide ou crypté, selon tes besoins
+        $manager->persist($anonymousUser);
+
+        // Créer 10 tâches de démonstration
+        for ($i = 0; $i < 10; $i++) {
+            $task = new Task();
+            $task->setTitle($faker->sentence(3));
+            $task->setContent($faker->paragraph());
+            $task->setCreatedAt($faker->dateTimeBetween('-1 years', 'now'));
+            $task->setIsDone($faker->boolean());  // Aléatoire : terminé ou non
+            $task->setAuthor($anonymousUser);     // Associer l'utilisateur "anonyme"
+            
+            $manager->persist($task);
+        }
+
+        // Sauvegarde de l'utilisateur et des tâches dans la base de données
+        $manager->flush();
+    }
+}
