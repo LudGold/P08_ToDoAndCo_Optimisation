a0410c4f6cb0d420e197bd89e9c6d419443a579b
Goldstein Ludivine <sneakymombox@gmail.com>
2024-12-04 13:50:55 +0100

    review quality code security

--- public/app_dev.php
+++ public/app_dev.php
@@ -1,30 +1,29 @@
 <?php
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Debug\Debug;
+use Symfony\Component\HttpFoundation\Response;
 
-// If you don't want to setup permissions the proper way, just uncomment the following PHP line
-// read http://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
-// for more information
-//umask(0000);
+// Vérifiez les adresses IP autorisées
+$allowedIps = ['127.0.0.1', '::1'];
+$clientIp = $_SERVER['REMOTE_ADDR'] ?? null;
 
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) || php_sapi_name() === 'cli-server')
+if (
+    isset($_SERVER['HTTP_CLIENT_IP']) ||
+    isset($_SERVER['HTTP_X_FORWARDED_FOR']) ||
+    ($clientIp && !in_array($clientIp, $allowedIps) && php_sapi_name() !== 'cli-server')
 ) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+    $response = new Response(
+        'You are not allowed to access this file.',
+        Response::HTTP_FORBIDDEN
+    );
+    $response->send();
+    return;
 }
 
-/** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../app/autoload.php';
-Debug::enable();
+// Chargez l'autoloader
+$loaderPath = __DIR__ . '/../app/autoload.php';
+if (!is_file($loaderPath)) {
+    throw new RuntimeException('The autoload file is missing.');
+}
+$loader = require $loaderPath;
 
-$kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+// Ajoutez d'autres initialisations ici si nécessaire
